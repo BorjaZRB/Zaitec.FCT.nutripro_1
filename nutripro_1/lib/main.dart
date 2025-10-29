423957223d421937feb6c5aad1ad6de6d73da77b
RicardMayol89 <rme0005@alu.medac.es>
2025-10-29 10:24:20 +0100

    THEME-001: aplicar tema global y paleta

--- nutripro_1/lib/main.dart
+++ nutripro_1/lib/main.dart
@@ -1,12 +1,9 @@
-
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:nutripro_1/firebase_options.dart';
 
 Future<void> main() async {
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-);
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
